e61078243e6dc806ef0d106d5932bf4c3ba48133
cobrabagaskara <60558256+cobrabagaskara@users.noreply.github.com>
2025-11-23 22:20:29 +0700

    Update auto-close-notification.js

--- modules/auto-close-notification.js
+++ modules/auto-close-notification.js
@@ -1,1 +1,18 @@
+console.log("[Module] auto-close-notification aktif!");
+
+(function() {
+    const observer = new MutationObserver(() => {
+        const notif = document.querySelector(".v-dialog .v-btn--icon");
+
+        if (notif) {
+            console.log("[Module] Menutup notifikasi...");
+            notif.click();
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
 
